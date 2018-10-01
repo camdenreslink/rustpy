c063207735970ab735ed017b927f4b11f91c8fbc
Camden Reslink <camdenreslink@gmail.com>
2018-09-30 23:52:40 -0400

    Added new workspace project organization.

--- rustpy_cli/src/main.rs
+++ rustpy_cli/src/main.rs
@@ -1,5 +1,3 @@
-mod tokenizer;
-
 fn main() {
     println!("Hello, world!");
 }
--- rustpy_compiler/src/tokenizer/mod.rs
+++ rustpy_compiler/src/tokenizer/mod.rs
@@ -11,7 +11,12 @@
 //! Design/Implementation
 //! ---------------------
 //! 
-//! This tokenizer is implemented as an LL(1) recursive descent parser. [^language-implementation-patterns-parr]
+//! This tokenizer is implemented as an LL(1) recursive descent parser [^language-implementation-patterns-parr]. One unique wrinkle in Python's tokenizer, is that it also handles some of the logic of significant whitespace specifying code blocks through the `INDENT` and `DEDENT` tokens.
+//! 
+//! Notes/Considerations
+//! --------------------
+//! 
+//! * As of Python 3.7.0 UTF-16 is not supported as a source encoding.
 //! 
 //! Relevant PEPs
 //! -------------
@@ -22,9 +27,15 @@
 //! Links to CPython Source Code
 //! ----------------------------
 //! 
-//! * [tokenizer.c](https://github.com/python/cpython/blob/master/Parser/tokenizer.c)
+//! * [tokenizer.c](https://github.com/python/cpython/blob/master/Parser/tokenizer.c) - Parsing loop is in `tok_get` function.
 //! * [tokenizer.h](https://github.com/python/cpython/blob/master/Parser/tokenizer.h)
 //! 
+//! Additional Resources
+//! --------------------
+//! 
+//! * [Unicode Literals in Python Source Code](https://docs.python.org/3/howto/unicode.html#unicode-literals-in-python-source-code)
+//! * [The Guts of Unicode in Python - Video](https://pyvideo.org/pycon-us-2013/the-guts-of-unicode-in-python.html)
+//!  
 //! [^language-implementation-patterns-parr]: [Language Implementation Patterns](https://pragprog.com/book/tpdsl/language-implementation-patterns) by Terence Parr; Pattern 2: LL(1) Recursive-Descent Lexer
 pub fn tokenize() {
     ()
